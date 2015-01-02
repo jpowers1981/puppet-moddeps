69938bfa57f586f4c932be0b7dc97ade9822171e
Jonathan Powers <jpowers@gmail.com>
2015-01-02 11:47:50 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,9 @@ require 'simplecov'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  CodeClimate::TestReporter::Formatter,
   Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
+  SimpleCov::Formatter::HTMLFormatter
 ]
 SimpleCov.start 'rails'
 
